0be5f8ca6936e596e72adcb8e9085d98188108ff
sgudz <vortexuelpm@gmail.com>
2019-10-08 16:22:12 +0300

    Update kill_containers.sh

--- kill_containers.sh
+++ kill_containers.sh
@@ -8,18 +8,24 @@ else
 fi
 
 for pod in $pods; do
-  pod_id=`kubectl get pod -n $namespace $pod -o jsonpath='{.status.containerStatuses[].containerID}' | cut -d '/' -f3;`
-  node=`kubectl get pod -n $namespace $pod -o wide | awk '/node/ {print $7}'`
-  node_ip=`kubectl get node -o wide $node | awk '/node/ {print $7}'`
-  shim_pid=$(ssh -i /home/ubuntu/.ssh/openstack_tmp -o StrictHostKeyChecking=no ubuntu@$node_ip "ps aux | grep $pod_id | grep -v grep | awk '{print \$2}'")
-  echo "Pod name: $pod"
-  echo "Pod ID: $pod_id"
-  echo "Node name: $node"
-  echo "Node ip: $node_ip"
-  echo "containerd-shim process ID: $shim_pid"
-  echo ""
-  if [ "$3" == "kill" ]; then
-    ssh -i /home/ubuntu/.ssh/openstack_tmp -o StrictHostKeyChecking=no ubuntu@$node_ip "sudo kill -9 $shim_pid"
-    echo "process $shim_pid for pod $pod is killed"
-  fi
+  containers_in_pod=`kubectl get pod -n $namespace $pod | grep Running | awk '{print $2}' | cut -d "/" -f2`
+  for i in $(seq 0 $(($containers_in_pod-1))); do
+    container_id=`kubectl get pod -n $namespace $pod -o jsonpath="{.status.containerStatuses[$i].containerID}" | cut -d '/' -f3;`
+    container_name=`kubectl get pod -n $namespace $pod -o jsonpath="{.status.containerStatuses[$i].name}"`
+    node=`kubectl get pod -n $namespace $pod -o wide | awk '/node/ {print $7}'`
+    node_ip=`kubectl get node -o wide $node | awk '/node/ {print $7}'`
+    shim_pid=$(ssh -i /home/ubuntu/.ssh/openstack_tmp -o StrictHostKeyChecking=no ubuntu@$node_ip "ps aux | grep $container_id | grep -v grep | awk '{print \$2}'")
+    echo "Pod name: $pod"
+    echo "Contaiiners in pod: $containers_in_pod"
+    echo "Containers name: $container_name"
+    echo "Container ID: $container_id"
+    echo "Node name: $node"
+    echo "Node ip: $node_ip"
+    echo "containerd-shim process ID: $shim_pid"
+    echo ""
+    if [ "$3" == "kill" ]; then
+      ssh -i /home/ubuntu/.ssh/openstack_tmp -o StrictHostKeyChecking=no ubuntu@$node_ip "sudo kill -9 $shim_pid"
+      echo "process $shim_pid for container $container_name in $pod pod is killed"
+    fi
+  done
 done
